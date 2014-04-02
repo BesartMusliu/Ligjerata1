f8a0b28c8a1038b1ecaa8fe1770a58f69f4e3bed
BesartMusliu <artmusliu@hotmail.com>
2014-04-02 20:17:56 +0200

    Update Projekti.java

--- src/Projekti.java
+++ src/Projekti.java
@@ -1,14 +1,29 @@
-
 public class Projekti {
-	public static void main (String[] args) {
-		
-		int i = 5;
-		int j = 10;
-		int sum = i + j;
-		double dbl = 0.0;
-		float flt = 0.0f;
-		char ch = 'a';
+	public static void main(String[] args) {
 
-		System.out.println("" + sum);
+		String str = "123456";
+
+		for (int i = 0; i < str.length(); ++i) {
+			if (i == 0) {
+				System.out.println(str.charAt(i));
+			} else if (i == 1) {
+				System.out.println("Hajdar Dushi");
+			} else {
+				System.out.println("Hanife Shtiklla");
+			}
+		}
+
+
+		int j = 0;
+
+		do {
+			// egzekutohet e para
+		} while (j < str.length());
+
+		while (j < str.length()) {
+			System.out.println(str.charAt(j));
+			++j;
+		}
+
 	}
 }
